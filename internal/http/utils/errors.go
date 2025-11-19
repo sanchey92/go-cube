bf2c07980606927b7213a3f0f3668ab6d98f5190
Alexandr <sashkaageev@gmail.com>
2025-11-20 00:52:43 +0300

    updated custom errors for http

--- internal/http/utils/errors.go
+++ internal/http/utils/errors.go
@@ -8,6 +8,9 @@ import (
 var (
 	ErrRequestBodyEmpty = errors.New("request body is empty")
 	ErrJSONEncoding     = errors.New("failed json encoding")
+	ErrInvalidTaskID    = errors.New("taskID is empty")
+	ErrTaskNotFound     = errors.New("task not found")
+	ErrInvalidTaskState = errors.New("task is not in a valid state for this operation")
 )
 
 type ErrResponse struct {
@@ -17,6 +20,18 @@ type ErrResponse struct {
 
 func BadRequest(err error) *ErrResponse {
 	return newErrResponse(http.StatusBadRequest, err)
+}
+
+func InternalServerErr(err error) *ErrResponse {
+	return newErrResponse(http.StatusInternalServerError, err)
+}
+
+func NotFound(err error) *ErrResponse {
+	return newErrResponse(http.StatusNotFound, err)
+}
+
+func Conflict(err error) *ErrResponse {
+	return newErrResponse(http.StatusConflict, err)
 }
 
 func newErrResponse(statusCode int, err error) *ErrResponse {
